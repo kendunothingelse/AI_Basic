937776b088fb78cae9865c8cf46f5e92f9b07f7c
Cyber Games <21130399@st.hcmuaf.edu.vn>
2023-10-02 08:55:50 +0700

    ex2

---
+++ src/lab1Ex2/AgentProgram.java
@@ -0,0 +1,29 @@
+package lab1Ex2;
+
+import lab1Ex2.Environment.LocationState;
+
+public class AgentProgram {
+
+	public Action execute(Percept p) {// location, status
+		// TODO
+		Action ac = null;
+		if (p.getLocationState() == LocationState.DIRTY) {
+			ac = Environment.SUCK_DIRT;
+		} else {
+			switch (p.getAgentLocation()) {
+			case Environment.LOCATION_A:
+				ac = Environment.MOVE_LEFT;
+				break;
+			case Environment.LOCATION_B:
+				ac = Environment.MOVE_RIGHT;
+				break;
+			default:
+				ac = NoOpAction.NO_OP;
+				break;
+			}
+		}
+
+		return ac;
+
+	}
+}
