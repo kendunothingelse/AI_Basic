c38abba3bf9617fc4cc5b68281b8f269db6304d2
kendunothingelse <146355870+kendunothingelse@users.noreply.github.com>
2023-10-06 11:17:03 +0700

    Update BreadthFirstSearchAlgo.java
    update bfs

--- src/lab2Ex1/BreadthFirstSearchAlgo.java
+++ src/lab2Ex1/BreadthFirstSearchAlgo.java
@@ -3,8 +3,25 @@ package lab2Ex1;
 public class BreadthFirstSearchAlgo implements ISearchAlgo {
 
 	@Override
-	public Node execute(Node root, String goal) {
+public Node execute(Node root, String goal) {
 		// TODO Auto-generated method stub
+		Queue<Node> frontier = new LinkedList<Node>();
+		List<Node> explore = new ArrayList<Node>();
+		frontier.add(root);
+		while (!frontier.isEmpty()) {
+			Node parent = frontier.poll();
+			if (parent.getLabel().contains(goal)) {
+				return parent;
+			}
+			explore.add(parent);
+			List<Node> children = new ArrayList<Node>();
+			for (Node child : children) {
+				if (!frontier.contains(child) && !explore.contains(child)) {
+					frontier.add(child);
+					child.setParent(parent);
+				}
+			}
+		}
 		return null;
 	}
 
