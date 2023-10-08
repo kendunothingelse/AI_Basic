a50b647474aedf855bd9dd4980c56c9dd56a10ac
kendunothingelse <21130399@st.hcmuaf.edu.vn>
2023-10-08 15:05:44 +0700

    update TestMain

--- src/lab2Ex1/BreadthFirstSearchAlgo.java
+++ src/lab2Ex1/BreadthFirstSearchAlgo.java
@@ -1,15 +1,20 @@
 package lab2Ex1;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 public class BreadthFirstSearchAlgo implements ISearchAlgo {
 
 	@Override
-public Node execute(Node root, String goal) {
+	public Node execute(Node root, String goal) {
 		// TODO Auto-generated method stub
 		Queue<Node> frontier = new LinkedList<Node>();
-		List<Node> explore = new ArrayList<Node>();
+		List<Node> explore = new ArrayList<Node>();// explore set to store the node
 		frontier.add(root);
 		while (!frontier.isEmpty()) {
-			Node parent = frontier.poll();
+			Node parent = frontier.poll();// remove the leaf node in frontier and make a parent node
 			if (parent.getLabel().contains(goal)) {
 				return parent;
 			}
