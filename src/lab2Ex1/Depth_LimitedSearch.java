9cc4b48a1a4fae074113e06e2867c13afcf1b36d
kendunothingelse <21130399@st.hcmuaf.edu.vn>
2023-10-13 11:27:15 +0700

    update depth limit search

---
+++ src/lab2Ex1/Depth_LimitedSearch.java
@@ -0,0 +1,43 @@
+package lab2Ex1;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class Depth_LimitedSearch implements ISearchAlgo {
+
+	@Override
+	public Node execute(Node root, String goal) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Node execute(Node root, String start, String goal) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Node execute(Node root, String goal, int limitedDepth) {
+		Stack<Node> frontier = new Stack();
+		List<Node> explored = new ArrayList<Node>();
+		while (!frontier.isEmpty()) {
+			Node current = frontier.pop();
+			if (current.getLabel().equals(goal)) {
+				return current;
+			} else {
+				if (current.getDepth() < limitedDepth) {
+					List<Node> children = current.getChildrenNodes();
+					for (Node node : children) {
+						if (!explored.contains(node) && !frontier.contains(node)) {
+							frontier.add(node);
+							node.setParent(current);
+							node.setDepth(current.getDepth() + 1);
+						}
+					}
+				}
+			}
+		}
+		return null;
+	}
+}
